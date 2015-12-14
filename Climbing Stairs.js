9f4f0b45638c92cfe633b7625d2045d1d3b0eb35
JasonSJ <xjtusunjie@gmail.com>
2015-12-14 23:07:37 +0800

    Create Climbing Stairs.js

---
+++ Climbing Stairs.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n) {
+    var dp = [];
+    if(n===0) {
+        return 1;
+    }
+    if(n===1) {
+        return 1;
+    }
+    dp[0] = 1;
+    dp[1] = 1;
+    for(var i = 2; i <= n;i++){
+        dp[i] = dp[i-1] + dp[i-2];
+    }
+    return dp[n];
+};
